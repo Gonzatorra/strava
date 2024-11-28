59cbb4d2b422b7b4f5596eedec82c5208c520a1a
enekobarod <eneko.barbadillo@opendeusto.es>
2024-11-28 09:28:12 +0100

    ServicioAuthMeta cambiado

--- src/main/java/es/deusto/sd/strava/auth/servicioAuthMeta.java
+++ src/main/java/es/deusto/sd/strava/auth/servicioAuthMeta.java
@@ -2,4 +2,17 @@ package es.deusto.sd.strava.auth;
 
 public class servicioAuthMeta extends servicioAuth {
 
+	@Override
+	public boolean validarToken(String token) {
+		// TODO Auto-generated method stub
+		return super.validarToken(token);
+	}
+
+	@Override
+	public String obtenerInfoUsuario(String token) {
+		// TODO Auto-generated method stub
+		return super.obtenerInfoUsuario(token);
+	}
+	
+
 }
